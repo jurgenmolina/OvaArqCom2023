cf18aa1fd2de5e4bfccce812a4c15881040ea9bf
jurgenmolina <jurgenhumbertomoba@ufps.edu.co>
2023-05-05 15:33:35 -0500

    Creación del proyecto - maven Spring Boot 3.0.6 Jar - Java 8

---
+++ src/main/java/com/ova/arq2023/Arq2023Application.java
@@ -0,0 +1,13 @@
+package com.ova.arq2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Arq2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Arq2023Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ova/arq2023/Arq2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ova.arq2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Arq2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
