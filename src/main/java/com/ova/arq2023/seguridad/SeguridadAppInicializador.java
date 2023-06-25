f1906f4b1a364b9a52dfe23b0fc86d43a75b62a2
jurgenmolina <jurgenhumbertomoba@ufps.edu.co>
2023-06-25 15:55:28 -0500

    UNIDAD 1 Y UNIDAD 4

---
+++ src/main/java/com/ova/arq2023/seguridad/SeguridadAppInicializador.java
@@ -0,0 +1,7 @@
+package com.ova.arq2023.seguridad;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SeguridadAppInicializador extends AbstractSecurityWebApplicationInitializer {
+
+}
