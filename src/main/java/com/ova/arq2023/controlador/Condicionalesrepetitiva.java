5616e89c0b76d0c2b5e7b2621bd19aa4324e4424
Vicmox <victormanuelms@ufps.edu.co>
2023-06-25 21:55:48 -0500

    unidad seis

--- src/main/java/com/ova/arq2023/controlador/Condicionalesrepetitiva.java
+++ src/main/java/com/ova/arq2023/controlador/Condicionalesrepetitiva.java
@@ -6,9 +6,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class Condicionalesrepetitiva {
 
-    @GetMapping("/condicionales-saltos-e-instrucciones-repetitivas")
-	public String Inicio() {
-		return "condicionales-saltos-e-instrucciones-repetitivas";
-	}
-    
+    @GetMapping("/unidad-seis")
+    public String Inicio() {
+        return "unidad-seis";
+    }
+
+    @GetMapping("/examen/unidad-seis")
+    public String Examen() {
+        return "unidad_seis/examen-unidad-seis";
+    }
+   
 }
+
+
