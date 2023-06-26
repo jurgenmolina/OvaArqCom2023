aebf51971debb07549fb3d18cb00cd901847ed50
Vicmox <victormanuelms@ufps.edu.co>
2023-06-26 07:03:31 -0500

    solucionando unidad siete

--- target/classes/static/js/preguntas.js
+++ target/classes/static/js/preguntas.js
@@ -1,91 +1,180 @@
 let questions = [
-  {
-    numb: 1,
-    question: "¿Cuál es una de las etiquetas utilizadas en lenguaje ensamblador para realizar comparaciones?",
-    answer: "CMP",
-    options: [
-      "ADD",
-      "MOV",
-      "SUB",
-      "CMP"
-    ]
-  },
-  {
-    numb: 2,
-    question: "¿Qué tipo de salto se utiliza en ensamblador para saltar a una etiqueta si la comparación anterior es verdadera?",
-    answer: "JNE",
-    options: [
-      "JE",
-      "JMP",
-      "JG",
-      "JNE"
-    ]
-  },
-  {
-    numb: 3,
-    question: "¿Cuál es la instrucción en ensamblador utilizada para llamar a un procedimiento?",
-    answer: "CALL",
-    options: [
-      "JMP",
-      "RET",
-      "CALL",
-      "PUSH"
-    ]
-  },
-  {
-    numb: 4,
-    question: "¿Qué instrucción se utiliza en ensamblador para realizar una iteración controlada por una condición?",
-    answer: "LOOP",
-    options: [
-      "INC",
-      "DEC",
-      "LOOP",
-      "JMP"
-    ]
-  },
-  {
-    numb: 5,
-    question: "¿Cuál es una de las instrucciones utilizadas en ensamblador para realizar una iteración controlada por un contador?",
-    answer: "LOOPNZ",
-    options: [
-      "LOOPZ",
-      "LOOPNZ",
-      "LOOPNE",
-      "LOOPE"
-    ]
-  },
-  {
-  numb: 6,
-  question: "¿Cuál es la instrucción utilizada en el lenguaje ensamblador para repetir un bloque de código un número específico de veces?",
-  answer: "LOOP",
+    {
+        numb: 1,
+        question: "¿Cuál es una de las etiquetas utilizadas en lenguaje ensamblador para realizar comparaciones?",
+        answer: "CMP",
+        options: [
+            "ADD",
+            "MOV",
+            "SUB",
+            "CMP"
+        ]
+    },
+    {
+        numb: 2,
+        question: "¿Qué tipo de salto se utiliza en ensamblador para saltar a una etiqueta si la comparación anterior es verdadera?",
+        answer: "JNE",
+        options: [
+            "JE",
+            "JMP",
+            "JG",
+            "JNE"
+        ]
+    },
+    {
+        numb: 3,
+        question: "¿Cuál es la instrucción en ensamblador utilizada para llamar a un procedimiento?",
+        answer: "CALL",
+        options: [
+            "JMP",
+            "RET",
+            "CALL",
+            "PUSH"
+        ]
+    },
+    {
+        numb: 4,
+        question: "¿Qué instrucción se utiliza en ensamblador para realizar una iteración controlada por una condición?",
+        answer: "LOOP",
+        options: [
+            "INC",
+            "DEC",
+            "LOOP",
+            "JMP"
+        ]
+    },
+    {
+        numb: 5,
+        question: "¿Cuál es una de las instrucciones utilizadas en ensamblador para realizar una iteración controlada por un contador?",
+        answer: "LOOPNZ",
+        options: [
+            "LOOPZ",
+            "LOOPNZ",
+            "LOOPNE",
+            "LOOPE"
+        ]
+    },
+    {
+        numb: 6,
+        question: "¿Cuál de los siguientes dispositivos es utilizado como almacenamiento portátil?",
+        answer: "Memorias auxiliares (USB)",
+        options: [
+            "Disco Duro",
+            "Unidad de estado sólido (SSD)",
+            "Memorias auxiliares (USB)",
+            "Memoria RAM"
+        ]
+    },
+    {
+        numb: 7,
+        question: "¿Cuál de los siguientes registros se utiliza como puntero fuente de datos en programación de ensamblador?",
+        answer: "ESI",
+        options: [
+            "EBX",
+            "EDI",
+            "ECX",
+            "ESI"
+        ]
+    },
+    {
+        numb: 8,
+        question: "¿Qué función cumple un buffer en el procesamiento de datos?",
+        answer: "Almacenar temporalmente datos durante su procesamiento",
+        options: [
+            "Enviar datos a un dispositivo de salida",
+            "Realizar cálculos matemáticos",
+            "Almacenar temporalmente datos durante su procesamiento",
+            "Recibir datos de un dispositivo de entrada"
+        ]
+    },
+    {
+        numb: 9,
+        question: "¿Cuál es la representación numérica del carácter 'A' en ASCII?",
+        answer: "65",
+        options: [
+            "65",
+            "66",
+            "67",
+            "97"
+        ]
+    },
+    {
+        numb: 10,
+        question: "¿Qué tipo de arquitectura se caracteriza por tener procesadores de 32 bits?",
+        answer: "Arquitectura de 32 bits",
+        options: [
+            "Arquitectura de 16 bits",
+            "Arquitectura de 32 bits",
+            "Arquitectura de 64 bits",
+            "Visualización de arquitectura"
+        ]
+    },
+    {
+        numb: 11,
+        question: "¿Cuál es el procesador de 16 bits de Intel?",
+        answer: "Intel 8086",
+        options: [
+            "AMD64",
+            "Intel 80386",
+            "Intel 8086",
+            "QEMU: Emulador de sistemas"
+        ]
+    },
+    {
+        numb: 12,
+        question: "¿Cuál es el procesador de 64 bits de AMD?",
+        answer: "AMD64",
+        options: [
+            "QEMU: Emulador de sistemas",
+            "Intel 8086",
+            "AMD64",
+            "GNU Debugger"
+        ]
+    }, {
+        numb: 13,
+        question: "¿Qué componente de la computadora ejecuta instrucciones y controla el flujo de datos?",
+        answer: "CPU",
+        options: [
+            "Memoria RAM",
+            "Disco duro",
+            "CPU",
+            "Placa madre"
+        ]
+    },
+    {
+        numb: 14,
+        question: "¿Cuál es el tipo de memoria volátil utilizada por la CPU para almacenar datos y programas mientras se están ejecutando?",
+        answer: "Memoria RAM",
+        options: [
+            "CPU",
+            "Memoria RAM",
+            "Disco duro",
+            "Puerto USB"
+        ]
+    },
+    {
+        numb: 15,
+        question: "¿Qué dispositivo de almacenamiento no volátil se utiliza para almacenar permanentemente datos y programas en la computadora?",
+        answer: "Disco duro",
+        options: [
+            "Placa madre",
+            "Tarjeta de red",
+            "Disco duro",
+            "Puerto USB"
+        ]
+    },{
+  numb: 16,
+  question: "¿Cuál de las siguientes afirmaciones describe una diferencia clave entre la memoria RAM y la memoria ROM?",
+  answer: "La memoria RAM es volátil, mientras que la memoria ROM es no volátil",
   options: [
-    "REPEAT",
-    "FOR EACH",
-    "LOOP",
-    "WHILE"
-  ]
-},
-{
-  numb: 7,
-  question: "¿Qué instrucción se utiliza en ensamblador para repetir un bloque de código hasta que se cumpla una condición?",
-  answer: "REPEAT",
-  options: [
-    "FOR EACH",
-    "LOOP",
-    "REPEAT",
-    "DO-WHILE"
-  ]
-},
-{
-  numb: 8,
-  question: "¿Cuál es la instrucción empleada en lenguaje ensamblador para iterar sobre una colección de elementos?",
-  answer: "FOR EACH",
-  options: [
-    "FOR",
-    "REPEAT",
-    "FOR EACH",
-    "WHILE"
+    "La memoria RAM es más rápida que la memoria ROM",
+    "La memoria RAM tiene una capacidad mayor que la memoria ROM",
+    "La memoria RAM es volátil, mientras que la memoria ROM es no volátil",
+    "La memoria RAM se accede secuencialmente, mientras que la memoria ROM se accede aleatoriamente"
   ]
 }
+
+
+
 ];
 
